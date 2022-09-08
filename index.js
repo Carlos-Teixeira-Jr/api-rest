f80ea164c48d1c45da0f891a5ad680f3f4701a38
Carlos-Teixeira-Jr <teixeirahist1988@gmail.com>
2022-09-08 16:56:13 -0300

    implementação de sistemade autenticação com jwt

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
+const JWTSecret = "12121212";
+
+
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+//MIDDLEWARE: função executada antes da rota especificada ser executada;
+function auth(req,res,next){
+  const authToken = req.headers["authorization"];
+
+  if (authToken != undefined) {
+    const bearer = authToken.split(" ");
+    let token = bearer[1];
+    jwt.verify(token,JWTSecret,(err,data)=>{
+      if (err) {
+        res.status(401);
+        res.json({err:"Token inválido"});
+      }else{
+        req.token = token;
+        req.loggedUser = {id: data.id,email: data.email};
+        next();
+      }
+    });
+  } else {
+    res.status(401);
+    res.json({err:"Token inválido"});
+  }
+
+  
+}
 
 let DB = {
   games:[
@@ -24,18 +55,32 @@ let DB = {
       title: "Minecraft",
       year: 2012,
       price: 20
+    },
+  ],
+  users:[
+    {
+      id: 1,
+      name: "Victor Lima",
+      email: "victordevtb@gmail.com",
+      password: "node"
+    },
+    {
+      id: 2,
+      name: "Camila",
+      email: "cah@gmail.com",
+      password: "express"
     }
   ]
 }
 
 //REST - rota de listagem de todos os itens:
-app.get("/games",(req,res)=>{//URL unifiorme e direta e método adequado para a finalidade da rota;
+app.get("/games", auth,(req,res)=>{//URL unifiorme e direta e método adequado para a finalidade da rota;
   res.statusCode = 200;//Retorna um código de estado da requisição;
-  res.json(DB.games);
+  res.json({user: req.loggedUser,games: DB.games});
 });
 
 //REST - rota de listagem de item individual;
-app.get("/game/:id",(req,res)=>{//URL no singular (pois a rota anterior, por se tratar de uma listagem de todos os itens, deve realmente estar no plural), uniforme e direta;
+app.get("/game/:id", auth,(req,res)=>{//URL no singular (pois a rota anterior, por se tratar de uma listagem de todos os itens, deve realmente estar no plural), uniforme e direta;
 
   if (isNaN(req.params.id)) {
     res.sendStatus(400);//Retorno de mensagem de status da requisição;
@@ -54,7 +99,7 @@ app.get("/game/:id",(req,res)=>{//URL no singular (pois a rota anterior, por se 
 })
 
 //REST - rota de criação de novo jogo no db;
-app.post("/game", (req,res)=>{//Aqui a rota precisa ser do tipo POST para indicar que se trata do método CREATE e o endereço url da rota precisa ser game no singular indicando apenas o tipo de item que será criado;
+app.post("/game", auth, (req,res)=>{//Aqui a rota precisa ser do tipo POST para indicar que se trata do método CREATE e o endereço url da rota precisa ser game no singular indicando apenas o tipo de item que será criado;
   let {title, price, year} = req.body;//Sintaxe do ecma script 6 para economizar digitação;
 
   DB.games.push({
@@ -68,7 +113,7 @@ app.post("/game", (req,res)=>{//Aqui a rota precisa ser do tipo POST para indica
 
 })
 
-app.delete("/game/:id",(req,res)=>{
+app.delete("/game/:id", auth,(req,res)=>{
   if (isNaN(req.params.id)) {
     res.sendStatus(400);
   } else {
@@ -120,6 +165,40 @@ app.put("/game/:id",(req,res)=>{
   }
 })
 
+//Rota de autenticação de usuários
+app.post("/auth",(req,res)=>{
+
+  let {email,password} = req.body;
+
+  if(email != undefined){
+
+    let user = DB.users.find(u => u.email == email);
+    if (user != undefined) {
+      if(user.password == password){
+
+        jwt.sign({id: user.id, email: user.email}, JWTSecret,{expiresIn: "48h"},(err,token)=>{
+          if (err) {
+            res.status(400);
+            res.json({err: "Falha interna"});
+          } else {
+            res.status(200);
+            res.json({token: token});
+          }
+        })
+      }else{
+        res.status(401);
+        res.json({err: "Credenciais inválidas"});
+      }
+    } else {
+      res.status(404);
+      res.json({err: "O email enviado não existe na base de dados"})
+    }
+  }else{
+    res.status(400);
+    res.json({err: "O email enviado não válido"})
+  }
+
+})
 
 app.listen(8080,()=>{
   console.log("api rodando");
